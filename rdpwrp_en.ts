da575a47ecfffd33a43eee3e7ec40633d32ae348
Ferenc Csiki <csikfer@gmail.com>
2016-09-07 13:06:38 +0200

    Lemaradt nyelvi fájlok.

---
+++ rdpwrp_en.ts
@@ -0,0 +1,434 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="dialog.ui" line="168"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Magyar nyelvre váltás.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Ha a gomb inaktív, akkor az aktuálisan kiválasztott nyelv a Magyar.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic; color:#ff8080;&quot;&gt;Nyelv váltáskor a billentyűzet kiosztás is megváltozik, és ez hatással van a terminál kliens vagy böngészőben használlt billentyű kiosztásra is!&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="188"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Angol nyelvre váltás.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Ha a gomb inaktív, akkor az aktuálisan kiválasztott nyelv az Angol.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic; color:#ff8080;&quot;&gt;Nyelv váltáskor a billentyűzet kiosztás is megváltozik, és ez hatással van a terminál kliens vagy böngészőben használlt billentyű kiosztásra is!&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="215"/>
+        <source>Tartomány</source>
+        <translation>Domain</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="222"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A windows tartomány kiválasztása.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="232"/>
+        <source>Kiszolgáló</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="239"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A terminál szerver kiválasztása a megadott tartományban.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="249"/>
+        <source>Felhasználó</source>
+        <translation>Account name</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="256"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A bejelentkezési név a megadott tartományban.&lt;/p&gt;&lt;p&gt;Ha egy másik tartományban aktív felhasználói névvel akarunk bejelentkezni a terminál szerverre, akkor itt megadhatjuk a tartományt a :&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;&amp;lt;tartomány&amp;gt;&lt;/span&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;\&lt;/span&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;&amp;lt;felhasználó&amp;gt;&lt;/span&gt; alakban.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="275"/>
+        <source>Jelszó</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="282"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A megadott felhasználóhoz tartozó jelszó.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; color:#ff8080;&quot;&gt;Úgyeljünk rá, hogy a kiválasztott nyelv alapján változik a billentyű kiosztás!&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="306"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Belépés a Windows terminál szerverre. A gomb csak akkor aktív, ha megadtuk a bejelentkezési nevet. és a jelszót is.&lt;/p&gt;&lt;p&gt;Vegyük figyelembe, hogy többszöri hibás jelszó után a szerver letilthatja egy időre a felhasználó belépését.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="309"/>
+        <source>Belépés a terminál szerverre</source>
+        <translation>Logon to the terminal server</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="347"/>
+        <source>Böngésző :</source>
+        <translation>Select Browser</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="354"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Böngésző kiválasztása, ha több böngésző is rendelkezésre áll.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="369"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A kiválasztott böngésző elindítása, a windows domain-ba való bejelentkezés nélkül.&lt;/p&gt;&lt;p&gt;A böngésző (ill. a teljes felhasználói környezet) minden egyes meleg vagy hideg start után alaphelyzetbe áll, minden jelszót vagy sütit elfelejt.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="372"/>
+        <source>Böngésző indítása</source>
+        <translation>Starting the browser</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="406"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A kliens gép kikapcsolása.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="409"/>
+        <source>Kikapcsol</source>
+        <translation>Power off</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="426"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A kliens gép újraindítása.  A kliens gépen minden változtatás, és adat törlődik.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="429"/>
+        <source>Újraindít</source>
+        <translation>Cold start</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="446"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Az ideiglenes felhasználói környezet eldobása, és ujra létrehozása.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="449"/>
+        <source>Meleg start</source>
+        <translation>Warm start</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="466"/>
+        <source>Segítség</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="535"/>
+        <source>Automatikus kikapcsolás </source>
+        <translation>Auto Power off</translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="558"/>
+        <source>másodperc tétlenség esetén.</source>
+        <translation>secs idleness</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="control.cpp" line="16"/>
+        <source>Hiba a UDPSocket bint(%1, %2) hívásban : %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control.cpp" line="55"/>
+        <source>E:%1
+A %2 sor %3 oszlopában
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="control.cpp" line="59"/>
+        <source>A hibás szöveg sor : </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletime.cpp" line="21"/>
+        <source>Az &quot;%1&quot; program idítása sikertelen.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletime.cpp" line="31"/>
+        <source>&quot;%1&quot; program időtullépés?</source>
+        <translation type="unfinished">&quot;%1&quot; program time out?</translation>
+    </message>
+    <message>
+        <location filename="idletime.cpp" line="38"/>
+        <source>xprintidle program Kimenete nem értelmezhető:&lt;br&gt;&lt;i&gt;
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletime.cpp" line="60"/>
+        <source>Nincs DISPLAY környezeti változó!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletime.cpp" line="67"/>
+        <source>A DISPLAY környezeti vátozó értéke nem értelmezhető : &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletime.cpp" line="92"/>
+        <source>Program hiba.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="47"/>
+        <source>Hiányos argumentum lista.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="56"/>
+        <source>Nem numerikus argumentum : &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="80"/>
+        <source>Nem állapítható meg a saját gép ip címe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="86"/>
+        <source>A megadott saját cím értelmezhetetlen : %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="98"/>
+        <source>&lt;br&gt;Kérem forduljon a rendszergazdához!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="99"/>
+        <source>Végzetes hiba!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="100"/>
+        <source>Figyelmeztetés</source>
+        <translation type="unfinished">Warning</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="111"/>
+        <source>Ismeretlen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="159"/>
+        <source>A konfigurációs fájl %1, nem olvasható, vagy nem létezik.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="167"/>
+        <source>A konfigurációs állomány tartalma nem értelmezhető.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="169"/>
+        <source>&lt;br&gt;A %1 állomány %2 sor %3 oszlopában :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="174"/>
+        <source>A hibás szöveg sor:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="39"/>
+        <source>Nem értelmezhető tftp szerver cím : %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="193"/>
+        <source>Hiba a bindSocket() hívásban : %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="209"/>
+        <source>did not send packet to host :(  %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="233"/>
+        <source>Incoming packet has invalid first byte (%1).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="263"/>
+        <source>error on incoming packet number %1 vs expected %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="270"/>
+        <source>Incoming packet returned invalid operation code (%1).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="283"/>
+        <source>did not send ack packet to host :(  %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tftp.cpp" line="291"/>
+        <source>No message received from host :(  %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>cIdleTimeOut</name>
+    <message>
+        <location filename="idletimeout.cpp" line="14"/>
+        <source>Az alkalmazás auomatikusan ki fog lépni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.cpp" line="15"/>
+        <source>másodperc múlva.
+
+Ha nem szertné, hogy a kilépjen,
+megnyomhatja az ESC gombot is.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.cpp" line="16"/>
+        <location filename="idletimeout.cpp" line="23"/>
+        <source>Ne!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.cpp" line="17"/>
+        <source>Kilépés.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.cpp" line="21"/>
+        <source>A terminál auomatikusan ki fog kapcsolni</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.cpp" line="22"/>
+        <source>másodperc múlva.
+
+Ha nem szertné, hogy kikapcsoljon a terminál,
+megnyomhatja az ESC gombot is.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.cpp" line="24"/>
+        <source>Kikapcsolás!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>idleTimeOut</name>
+    <message>
+        <location filename="idletimeout.ui" line="20"/>
+        <source>Figyelmeztetés</source>
+        <translation type="unfinished">Warning</translation>
+    </message>
+    <message>
+        <location filename="idletimeout.ui" line="60"/>
+        <source>up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="idletimeout.ui" line="85"/>
+        <source>down</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mainDialog</name>
+    <message>
+        <location filename="dialog.cpp" line="116"/>
+        <source>nincs</source>
+        <translation>no</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="319"/>
+        <location filename="dialog.cpp" line="431"/>
+        <source>Program hiba : %1</source>
+        <translation>Program error : %1</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="368"/>
+        <source>&lt;h1&gt;Parancs, vagy parancs idítási hiba&lt;/h1&gt;
+</source>
+        <translation>&lt;h1&gt;Command or start Command error&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="370"/>
+        <source>A program idítása nem lehetséges.
+</source>
+        <translation>Starting the program is not possible.</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="371"/>
+        <source>A program összeomlott.
+</source>
+        <translation>The program collapsed.</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="372"/>
+        <source>Idő tullépés.
+</source>
+        <translation>Time out</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="373"/>
+        <source>Program írási hiba.
+</source>
+        <translation>Process write erroro.
+</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="374"/>
+        <source>Program olvasási hiba.
+</source>
+        <translation>Process read errror.</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="376"/>
+        <source>Ismeretlen hiba.
+</source>
+        <translation>Unknown error</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="378"/>
+        <source>&lt;h2&gt;Parancs sor:&lt;/h2&gt;
+</source>
+        <translation>&lt;h2&gt;Comman line : &lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="381"/>
+        <location filename="dialog.cpp" line="407"/>
+        <source>&lt;h2&gt;A programnak nem volt kimenete.&lt;/h2&gt;
+</source>
+        <translation>&lt;h2&gt;The program was not output&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="384"/>
+        <location filename="dialog.cpp" line="410"/>
+        <source>&lt;h2&gt;A program kimenete:&lt;/h2&gt;
+</source>
+        <translation>&lt;h2&gt;The program output :&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="404"/>
+        <source>&lt;h1&gt;A parancs futási ideje gyanús. Hiba történt?&lt;/h1&gt;
+</source>
+        <translation>&lt;h1&gt;The command is suspected of running time. An error occurred?&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="405"/>
+        <source>&lt;h2&gt;Kilépési kód : %1</source>
+        <translation>&lt;h2&gt;Program return code : &lt;/h2&gt;</translation>
+    </message>
+</context>
+</TS>
